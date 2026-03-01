6881e47f47d4cc22212bb47a5da1018684a81192
Meikouz <121649289+Meikouz@users.noreply.github.com>
2026-03-01 20:02:03 +0200

    Add files via upload

---
+++ FactoryClient/FactoryClient.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include "FactoryLib.h"
+
+void UseFactory(IReportCreator* creator)
+{
+    IReport* report = creator->CreateReport();
+    report->Render();
+
+    // Важливо: звільнення через Release(), щоб delete був у DLL
+    report->Release();
+    creator->Release();
+}
+
+int main(){
+	setlocale(LC_ALL, "Ukr");
+
+    std::cout << "Демонстрація патерну 'Фабричний метод' (DLL).\n";
+
+    UseFactory(CreateTextReportCreator());
+    UseFactory(CreateHtmlReportCreator());
+    UseFactory(CreatePdfReportCreator());
+
+    std::cout << "Готово.\n";
+    return 0;
+}
